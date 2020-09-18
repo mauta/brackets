31621e84ccbadb9f6c70d4fd47eb2b839aa6a0d7
mauta <anna.opareva@gmail.com>
2020-09-18 08:25:24 +0300

    fear: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let sample = [];
+    bracketsConfig.forEach(el => sample.push(el[0].concat(el[1])));;
+    let temp = str;
+    let a;
+    for (let i = 0; i < sample.length; i++) {
+        let b = sample[i];
+        a = temp.replace(b, '');
+        temp = a;
+    }
+
+    if (temp === str) {
+        console.log('непара');
+        console.log(temp);
+        return false;
+    } else if (temp === '') {
+        console.log('пара');
+        console.log(temp);
+        return true;
+    } else return check(temp, bracketsConfig); 
 }
